acf7bfde52d2709719cf2bcb775ea96c2fe1f9d6
Aliakbar1974 <ifaz2001@gmail.com>
2026-09-24 23:49:20 +0600

    Fix quiz sound asset paths

--- Bangladesh-Quiz-11/script.js
+++ Bangladesh-Quiz-11/script.js
@@ -19,13 +19,13 @@ const FACEBOOK_POST_URL = "https://www.facebook.com/photo/?fbid=1221122196814357
 // ============================================================
 
 const sounds = {
-    start: new Audio("Sounds/Start.wav"),
-    correct: new Audio("Sounds/Correct.wav"),
-    wrong: new Audio("Sounds/Wrong.wav"),
-    tryAgain: new Audio("Sounds/TryAgain.wav"),
-    success: new Audio("Sounds/Success.wav"),
-    victory: new Audio("Sounds/Victory.wav"),
-    complete: new Audio("Sounds/Complete.wav")
+    start: new Audio("../Sounds/Start.wav"),
+    correct: new Audio("../Sounds/Correct.wav"),
+    wrong: new Audio("../Sounds/Wrong.wav"),
+    tryAgain: new Audio("../Sounds/TryAgain.wav"),
+    success: new Audio("../Sounds/Success.wav"),
+    victory: new Audio("../Sounds/Victory.wav"),
+    complete: new Audio("../Sounds/Complete.wav")
 };
 
 
